5942b96069443d7c9e80189a6637feec61641b1a
lauritsalstad-png <lauritsalstad@icloud.com>
2025-10-20 12:58:38 +0200

    Update Kontaktview.js

---
+++ View/Kontaktview.js
@@ -0,0 +1,18 @@
+KontaktView()
+function KontaktView(){
+    document.getElementById("app").innerHTML = 
+    `<div class="registrationbackground">
+    <div class="center">
+        <br><br><br><br>
+        <p>Kontakt oss</p>
+        <br><br>
+        <h3>Admin</h3>
+        <br>
+        <h2>AdminEmail@shadyapps.ru</h2>
+        <br><br>
+        <h3>Disc ansvarllig</h3>
+        <br>
+        <h2>DiscAnsvarligemail@shadyapps.ru</h2>
+    </div>
+</div>`
+}
