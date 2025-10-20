b64bdfdd26ab35463ca0fc783c91292fd920f284
MS12213312 <sadaf1231236da2@outlook.com>
2025-10-20 14:31:00 +0200

    ..

---
+++ View/Loginview.js
@@ -0,0 +1,30 @@
+function updateViewLogin(){
+    let html = /*html*/ `
+        <div>
+        ${createBanner()}
+        <br>
+        Username <br>
+        <input 
+            type="text"
+            ><br>
+        Password <br>
+        <input 
+            type="text"
+            ><br>
+
+        <button type="button" onclick="login()">Login</button>
+        <button type="button" onclick="model.app.currentPage='${'registerPage'}' updateView()">Register</button><br><br>
+        <button type="button" onclick="model.app.currentPage='${'contactPage'}' updateView()">Kontakt oss</button>
+        </div>
+        `;
+    document.getElementById("app").innerHTML = html;
+    };
+
+    function createBanner(){
+        return /*html*/ `
+        <div>
+            Banner
+            <div>Logo</div>
+        </div>
+        `;
+    };
